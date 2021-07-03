e431d1be0fbdeb38e975e5184e99941fc7909788
Wally Wissner <Wally.Wissner.MS@gmail.com>
2021-07-02 20:20:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,29 @@
+import pathlib
 from setuptools import setup
 
 
 version = "0.0.1"
 
 
+here = pathlib.Path(__file__).parent.resolve()
+
+
 setup(
     name="ml-report",
-    version = version,
-    description="Automated reporting for training Machine Learning models.",
-    long_description="TODO",
+    version=version,
+    description="Automated reporting for training Machine Learning models",
+    long_description=(here / 'README.md').read_text(),
+    long_description_content_type='text/markdown',
     author="Wally Wissner",
-    author_email="Wally.Wissner.MS@gmail.com",
-    url="https://github.com/wally-wissner/MLReport",
-    license="",
+    author_email="wally.wissner.MS@gmail.com",
+    url="https://github.com/wally-wissner/ml_report",
+    license=(here / 'LICENSE.txt').read_text(),
     install_requires=[
         "setuptools",
     ],
+    classiiers=[
+        'Development Status :: 3 - Alpha',
+    ]
 )
 
 # test.pypi.org
