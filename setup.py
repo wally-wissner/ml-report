2c3bd0fb0a98d120eef04e7afd75558e974868b4
Wally Wissner <Wally.Wissner.MS@gmail.com>
2021-07-02 21:05:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import pathlib
 from setuptools import setup
 
 
-version = "0.0.1"
+__version__ = "0.0.1"
 
 
 here = pathlib.Path(__file__).parent.resolve()
@@ -10,7 +10,7 @@ here = pathlib.Path(__file__).parent.resolve()
 
 setup(
     name="ml-report",
-    version=version,
+    version=__version__,
     description="Automated reporting for training Machine Learning models",
     long_description=(here / 'README.md').read_text(),
     long_description_content_type='text/markdown',
@@ -22,9 +22,23 @@ setup(
         "setuptools",
     ],
     classiiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 2 - Pre-Alpha'
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development',
     ],
     package_dir={'': "ml_report"},
+    python_requires=">=3.7",
 )
 
 # test.pypi.org
